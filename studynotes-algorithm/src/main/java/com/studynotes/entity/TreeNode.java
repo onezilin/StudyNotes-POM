b6eb811809b942fd4b8c924c7330413352daf184
onezilin <onezilinil@gmail.com>
2024-04-16 16:44:52 +0800

    ADD：添加 studynotes-algorithm 模块

---
+++ studynotes-algorithm/src/main/java/com/studynotes/entity/TreeNode.java
@@ -0,0 +1,49 @@
+package com.studynotes.entity;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Description:
+ */
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return toString(this);
+    }
+
+    private String toString(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        StringBuilder sb = new StringBuilder();
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node == null) {
+                sb.append("null,");
+            } else {
+                sb.append(node.val).append(",");
+                queue.offer(node.left);
+                queue.offer(node.right);
+            }
+        }
+        return sb.toString();
+    }
+}
